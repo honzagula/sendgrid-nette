74eb9c4b0786da142752dfc6248b6907fda59335
Jan Gula <honza.gula@gmail.com>
2016-06-03 19:36:15 +0200

    SendgridMailer implementation

---
+++ src/SendgridMailer.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Istrix\MailSender;
+
+use Nette\Mail\IMailer;
+use Nette\Mail\Message;
+use Nette\Object;
+use SendGrid;
+use SendGrid\Email;
+
+class SendgridMailer extends Object implements IMailer
+{
+	const ENDPOINT = "https://api.sendgrid.com/";
+
+	/** @var  string */
+	private $key;
+
+	/**
+	 * MailSender constructor
+	 *
+	 * @param string $key
+	 */
+	public function __construct($key)
+	{
+		$this->key = $key;
+	}
+
+	/**
+	 * @param string $key
+	 */
+	public function setKey($key)
+	{
+		$this->key = $key;
+	}
+
+	/**
+	 * Sends email to sendgrid
+	 *
+	 * @param Message $message
+	 * @throws SendGrid\Exception
+	 */
+	public function send(Message $message)
+	{
+		$sendGrid = new SendGrid($this->key);
+		$email = new Email();
+
+		$from = $message->getFrom();
+		reset($from);
+		$key = key($from);
+
+		$email->setFrom($key)
+			->setFromName($from[$key])
+			->setSubject($message->getSubject())
+			->setText($message->getBody())
+			->setHtml($message->getHtmlBody());
+
+		foreach($message->getHeader('To') as $recipient => $name) {
+			$email->addTo($recipient);
+		}
+
+		$sendGrid->send($email);
+	}
+
+}
